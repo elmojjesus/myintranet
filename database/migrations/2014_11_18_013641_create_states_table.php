ae231936dac5ff9640cb0623fa0393f53be0a360
Ueslei Lima <ueslei000@hotmail.com>
2016-04-11 10:56:37 -0300

    Commit

--- database/migrations/2014_11_18_013641_create_states_table.php
+++ database/migrations/2014_11_18_013641_create_states_table.php
@@ -26,6 +26,7 @@ class CreateStatesTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('states');
     }
 }
