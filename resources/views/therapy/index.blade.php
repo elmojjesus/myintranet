5c3a8d15b4d15726baef7146d8f483eab9330a6a
Alan96 <alan_aic@hotmail.com>
2016-05-02 11:02:32 -0300

    Layouts views

--- resources/views/therapy/index.blade.php
+++ resources/views/therapy/index.blade.php
@@ -1,33 +1,64 @@
 @extends('layouts.layout')
-@section('content')
-	<a data-mfp-src="/therapy/create" class="btn btn-primary modal-ajax-link">Adicionar Nova Terapia</a>
-	@if($therapies)
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Ações</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($therapies as $terapy)
-					<tr>
-						<td>{{ $terapy->name }}</td>
-						<td>
-							<button class="btn btn-link modal-ajax-link" data-mfp-src="/therapy/edit/{{ $terapy->id }}"><i class="fa fa-pencil"></i></button>
-							<button class="btn btn-link modal-ajax-link" data-mfp-src="/therapy/delete/{{ $terapy->id }}"><i class="fa fa-trash">Excluir</i></button>
-						</td>
-					</tr>
-				@endforeach
-			</tbody>
-			<tfoot>
-				<tr>
-					<th>Nome</th>
-					<th>Ações</th>
-				</tr>
-			</tfoot>
-		</table>
-	@else
 
-	@endif
-@endsection
+@section('title')
+    Controle terapia <small> / Pacientes / Terapias </small>
+@stop
+
+@section('content')
+    
+    <div class="row">
+        
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"> Cadastre uma terapia </div>
+                <div class="panel-body">
+                        {!! Form::open(array('method' => 'post', 'action' => 'SportController@store')) !!}
+                            <div class="form-group">
+                                <label> Nome da terapia </label>
+                                {!! Form::text('name', '', $attributes = array('class' => 'form-control')) !!}
+                                <br>
+                                {!! Form::submit('Cadastrar terapia', $attributes = array('class' => 'btn btn-primary')) !!}    
+                            </div>
+                        {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"> Terapias já registradas </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        
+                          <table class="table table-striped table-bordered table-hover">
+                            <tr>
+                                <th width="60%">Terapia</th>
+                                <th width="20%">Editar</th>
+                                <th width="20%">Remover</th>
+                            </tr>
+
+                                    <tr>
+                                        <td> Banheira</td>
+                                        <td style="text-align: center;"> 
+                                                 <a class="modal-ajax-link" > 
+                                                     <i class="fa fa-pencil"></i> 
+                                                 </a> 
+                                            </td>
+                                        <td style="text-align: center;">
+												<a class="modal-ajax-link">
+													<i class="fa fa-trash-o"></i>
+												</a>
+											</td>
+                                    </tr>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Fim linha -->
+    </div>
+        
+@stop
